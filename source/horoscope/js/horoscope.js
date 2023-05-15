65b4d36933fa20d89628cf8134f94dee196d7d60
Jacob Felts <jfelts@ucsd.edu>
2023-05-15 00:29:20 -0700

    Added the change in image to the horoscope sign.

--- source/horoscope/js/horoscope.js
+++ source/horoscope/js/horoscope.js
@@ -7,19 +7,19 @@ function init() {
     function date_to_horoscope(dateString) {
         // Array of signs. Capricorn is repeated since it crosses the new year
         const zodiacSigns = [
-            { name: "Capricorn", start: "01-01", end: "01-19" },
-            { name: "Aquarius", start: "01-20", end: "02-18" },
-            { name: "Pisces", start: "02-19", end: "03-20" },
-            { name: "Aries", start: "03-21", end: "04-19" },
-            { name: "Taurus", start: "04-20", end: "05-20" },
-            { name: "Gemini", start: "05-21", end: "06-20" },
-            { name: "Cancer", start: "06-21", end: "07-22" },
-            { name: "Leo", start: "07-23", end: "08-22" },
-            { name: "Virgo", start: "08-23", end: "09-22" },
-            { name: "Libra", start: "09-23", end: "10-22" },
-            { name: "Scorpio", start: "10-23", end: "11-21" },
-            { name: "Sagittarius", start: "11-22", end: "12-21" },
-            { name: "Capricorn", start: "12-22", end: "12-31" },
+            { name: "capricorn", start: "01-01", end: "01-19" },
+            { name: "aquarius", start: "01-20", end: "02-18" },
+            { name: "pisces", start: "02-19", end: "03-20" },
+            { name: "aries", start: "03-21", end: "04-19" },
+            { name: "taurus", start: "04-20", end: "05-20" },
+            { name: "gemini", start: "05-21", end: "06-20" },
+            { name: "cancer", start: "06-21", end: "07-22" },
+            { name: "leo", start: "07-23", end: "08-22" },
+            { name: "virgo", start: "08-23", end: "09-22" },
+            { name: "libra", start: "09-23", end: "10-22" },
+            { name: "scorpio", start: "10-23", end: "11-21" },
+            { name: "sagittarius", start: "11-22", end: "12-21" },
+            { name: "capricorn", start: "12-22", end: "12-31" },
         ];
 
         // Find the correct sign object.
@@ -53,6 +53,10 @@ function init() {
         //switch sign display text
         let sign_display = document.getElementById("sign-display");
         sign_display.textContent = "Your sign is: " + sign;
+
+        //switch sign display image
+        let sign_image = document.getElementById("sign-image");
+        sign_image.src = "https://symbolikon.com/wp-content/uploads/edd/2019/09/astrology-" + sign + "-bold-400w.png"
         
         let date_display = document.getElementById("date-display");
         date_display.textContent = "Today is " + new Date().toLocaleDateString();
