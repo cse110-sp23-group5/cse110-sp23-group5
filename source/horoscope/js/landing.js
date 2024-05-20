8cfa591a2c48d44e0692ab01c6440ed320015e47
akann0 <akann@ucsd.edu>
2024-05-20 13:51:41 -0700

    slight refactoring

--- source/horoscope/js/landing.js
+++ source/horoscope/js/landing.js
@@ -1,3 +1,5 @@
+import { dateToHoroscope } from './horoscope.js';
+
 const categoryElement = document.getElementById('category');
 const birthdayElement = document.getElementById('birthday-input');
 const HOROSCOPE_PAGE = 'horoscope.html';
@@ -47,23 +49,22 @@ function button() {
  * @returns {boolean} true if valid, false otherwise
  */
 function checkValidBirthday(birthday) {
+    check = true;
     if (birthday.length != 10 || birthday[4] != '-' || birthday[7] != '-') {
         alert("Please enter a valid date in the format MM/DD/YYYY");
-        return false;
     }
-    if (isNaN(Date.parse(birthday))) {
+    else if (isNaN(Date.parse(birthday))) {
         alert("Please enter a valid date in the format MM/DD/YYYY");
-        return false;
     }
-    if (new Date(birthday) > new Date()) {
+    else if (new Date(birthday) > new Date()) {
         alert("Please enter a valid date in the past");
-        return false;
     }
-    if (new Date(birthday) < new Date("1900-01-01")) {
+    else if (new Date(birthday) < new Date("1900-01-01")) {
         alert("Please enter a valid date after 1900");
-        return false;
+    } else {
+        return true;
     }
-    return true;
+    return false;
 }
 
 /**
@@ -90,43 +91,4 @@ function checkValidCategory(category) {
         return false;
     }
     return true;
-}
-
-/**
-     * Determines horoscope sign based on birthday
-     * @param {string} dateString string representation of the date in form YYYY-MM-DD
-     * @returns name of the zodiac sign
-     */
-function dateToHoroscope(dateString) {
-    // Array of signs. Capricorn is repeated since it crosses the new year
-    const zodiacSigns = [
-        { name: "Capricorn", start: "01-01", end: "01-19" },
-        { name: "Aquarius", start: "01-20", end: "02-18" },
-        { name: "Pisces", start: "02-19", end: "03-20" },
-        { name: "Aries", start: "03-21", end: "04-19" },
-        { name: "Taurus", start: "04-20", end: "05-20" },
-        { name: "Gemini", start: "05-21", end: "06-20" },
-        { name: "Cancer", start: "06-21", end: "07-22" },
-        { name: "Leo", start: "07-23", end: "08-22" },
-        { name: "Virgo", start: "08-23", end: "09-22" },
-        { name: "Libra", start: "09-23", end: "10-22" },
-        { name: "Scorpio", start: "10-23", end: "11-21" },
-        { name: "Sagittarius", start: "11-22", end: "12-21" },
-        { name: "Capricorn", start: "12-22", end: "12-31" },
-    ];
-
-    // Find the correct sign object.
-    // Date is 1 day off when printing to console because of time zone conversions.
-    const date = new Date(dateString); 
-    const year = dateString.substring(0,4);
-
-    const matchingSign = zodiacSigns.find(sign => {
-        const start = new Date(`${year}-${sign.start}`).getTime();
-        const end = new Date(`${year}-${sign.end}`).getTime();
-        const birthdayTime = date.getTime();
-        return (birthdayTime >= start && birthdayTime <= end);
-      });
-    
-    // Check if a sign is found
-    return matchingSign ? matchingSign.name : "NO SIGN FOUND";
 }
